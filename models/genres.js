7a0b2dc33d8aeb3c58e83b20a5850e2532758e94
Riswan Agyeman <agyeman.riswan@gmail.com>
2021-05-17 19:19:13 -0500

    Added a genreSchema Database

--- models/genres.js
+++ models/genres.js
@@ -1,13 +1,9 @@
 const { model } = require("mongoose");
 
-const mongoose = require(mongoose);
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const genreSchema = new Schema({
-    id: {
-        type: Number,
-        required: true
-    },
     year: {
         type: Number,
         required: true
